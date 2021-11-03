a62a5a247b417f2f12ae9b3c4c78d27efba27581
Chivova <Chivovaz@ua.fm>
2021-11-03 12:58:29 +0200

    change some files, add phonebook-operations.js

--- src/components/redux/phonebook-actions.js
+++ src/components/redux/phonebook-actions.js
@@ -1,16 +1,21 @@
 import { createAction } from '@reduxjs/toolkit';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
-const addContact = createAction('phonebook/Add', (name, number) => ({
-  payload: {
-    id: uuidv4(),
-    name,
-    number,
-  },
-}));
+// const addContact = createAction('phonebook/Add', (name, number) => ({
+//   payload: {
+//     id: uuidv4(),
+//     name,
+//     number,
+//   },
+// }));
 
-const deleteContact = createAction('phonebook/Delete');
+// const deleteContact = createAction('phonebook/Delete');
 
-const filterContact = createAction('phonebook/Filter');
+// const filterContact = createAction('phonebook/Filter');
 
-export { addContact, deleteContact, filterContact };
+// export { addContact, deleteContact, filterContact };
+const fetchContactsRequest = createAction('contacts/fetchContactsRequest');
+const fetchContactsSuccess = createAction('contacts/fetchContactsSuccess');
+const fetchContactsError = createAction('contacts/fetchContactsError');
+
+export { fetchContactsRequest, fetchContactsSuccess, fetchContactsError };
